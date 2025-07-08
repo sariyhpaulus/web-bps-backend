fa1947b4527e47b5f87107943cbaae090fdf93ad
sariyhpaulus <222112353@stis.ac.id>
2025-07-08 16:18:18 +0700

    handle cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // Tambahkan middleware Sanctum di sini jika perlu
+            \Illuminate\Http\Middleware\HandleCors::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
