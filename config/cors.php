1ac123cc006ac5b9e4e0b8f6633a6e2081a332bd
sariyhpaulus <222112353@stis.ac.id>
2025-07-12 20:26:26 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://web-bps-frontend.vercel.app'],
-    'allowed_origins_ patterns' => [],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
