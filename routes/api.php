8d2ea25446e0296095593adc27a755a5dc973316
sariyhpaulus <222112353@stis.ac.id>
2025-07-12 20:52:45 +0700

    try harder

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,30 @@ use App\Http\Controllers\PublikasiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Debug routes
+Route::get('/debug', function () {
+    return response()->json([
+        'message' => 'API routes are working!',
+        'timestamp' => now(),
+        'app_env' => config('app.env'),
+        'app_debug' => config('app.debug'),
+        'laravel_version' => app()->version()
+    ]);
+});
+
+Route::get('/routes', function () {
+    $routes = [];
+    foreach (Route::getRoutes() as $route) {
+        $routes[] = [
+            'method' => implode('|', $route->methods()),
+            'uri' => $route->uri(),
+            'name' => $route->getName(),
+            'action' => $route->getActionName()
+        ];
+    }
+    return response()->json(['routes' => $routes]);
+});
+
 // Test CORS route
 Route::get('/test-cors', function () {
     return response()->json([
